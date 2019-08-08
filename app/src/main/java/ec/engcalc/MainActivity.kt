8cfa2d1bc08da5d7d97246c72b2f94a5614c0b8e
rklogan <ryan.k.logan@gmail.com>
2019-08-08 15:52:26 -0400

    Bug Fix: Crashing on expressions of the form (x)

--- app/src/main/java/ec/engcalc/MainActivity.kt
+++ app/src/main/java/ec/engcalc/MainActivity.kt
@@ -2,14 +2,12 @@ package ec.engcalc
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
     }
 }
